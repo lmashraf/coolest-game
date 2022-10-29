77776ce69ac1f9c787b255ab0d9a1ea3e98c2a94
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2022-10-29 18:35:21 +0200

    Added admin user vars to access Jenkins instance into the docker run

--- jenkins/run_jenkins.sh
+++ jenkins/run_jenkins.sh
@@ -10,8 +10,10 @@ if [ ! -d "${JENKINS_HOME}" ]; then mkdir ${JENKINS_HOME}; fi
 docker build -t jenkins:jcasc .
 
 # Run Jenkins on port 8080
-docker run --name jenkins --rm -p 8080:8080 \
-           -v $(pwd)/${JENKINS_HOME}:/var/jenkins_home \
-           -v $(pwd)/${WORKDIR}:/${WORKDIR} \
-           -v /var/run/docker.sock:/var/run/docker.sock \
-           jenkins:jcasc
+docker run --name jenkins-coolest-game \
+            --rm -p 8080:8080 \
+            --env JENKINS_ADMIN_ID="admin" --env JENKINS_ADMIN_PASSWORD="p@55w0rd" \
+            -v $(pwd)/${JENKINS_HOME}:/var/jenkins_home \
+            -v $(pwd)/${WORKDIR}:/${WORKDIR} \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            jenkins:jcasc
