1b71c5841c6e2caa593d3308904e573c10a7c9a4
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2022-10-30 12:04:53 +0100

    Seed jobs from jcasc config through a remote git repo

--- jenkins/seed_jobs.groovy
+++ jenkins/seed_jobs.groovy
@@ -1,17 +1,22 @@
 job('seed_jobs') {
-    scm {
+    /*scm {
         // Alternatives in handling credentials in a better manner:
         // https://github.com/jenkinsci/job-dsl-plugin/blob/master/docs/Handling-Credentials.md
         git {
             remote {
-                url('https://${personal_token}/lmashraf/coolest-game-jobs-dsl/')
+                url('https://${personal_token}:/lmashraf/coolest-game-jobs-dsl/')
             }
             branch('main')
         }
-    }
+    }*/
     steps {
+        // Clone jobs
+        shell('''
+            wget https://github.com/lmashraf/coolest-game-jobs-dsl/archive/master.zip
+            unzip master.zip -d jobs/
+            ''')
         dsl {
-            external('*.groovy')
+            external('jobs/*.groovy')
         }
     }
 }
