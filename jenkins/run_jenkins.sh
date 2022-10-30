5f2391f5f13b8f409d88ca129400e65f2407dc9f
Mohammed Achraf Lebrazi <mohammedachraf.lebrazi@here.com>
2022-10-30 02:20:50 +0200

    Added docker mapping to host docker

--- jenkins/run_jenkins.sh
+++ jenkins/run_jenkins.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export JENKINS_HOME="jenkins_data"
-export WORKDIR="build"
 
 # Create a local Jenkins data folder to persist its configuration
 if [ ! -d "${JENKINS_HOME}" ]; then mkdir ${JENKINS_HOME}; fi
@@ -11,9 +10,10 @@ docker build -t jenkins:jcasc .
 
 # Run Jenkins on port 8080
 docker run --name jenkins-coolest-game \
+            --privileged \
             --rm -p 8080:8080 \
             --env JENKINS_ADMIN_ID="admin" --env JENKINS_ADMIN_PASSWORD="p@55w0rd" \
             -v $(pwd)/${JENKINS_HOME}:/var/jenkins_home \
-            -v $(pwd)/${WORKDIR}:/${WORKDIR} \
+            -v $(which docker):/usr/bin/docker \
             -v /var/run/docker.sock:/var/run/docker.sock \
             jenkins:jcasc
